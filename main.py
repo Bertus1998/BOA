db059e6a280e3767f08ebd4d2bbf8eed08c7aa32
Adrian Zielinski <adrianziel24@wp.pl>
2021-05-07 20:52:46 +0200

    Small adjustments

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
 import math
 import random
-
 import numpy as np
-from openpyxl import Workbook
-
-c = 0.1
-a = 0.5
-
-
-def const_function(start, end, coeff):
-    return start + end / 2
 
 
 # od 0 do 1,
@@ -69,55 +60,55 @@ def leeyao_func(x: np.array):
 
 
 class Butterfly:
-    def __init__(self, func, dimensions: int, x_from: int, x_to: int):
+    def __init__(self, func, dimensions: int, x_from: int, x_to: int, a: float, c: float):
         self.func = func
         self.position = np.random.uniform(low=x_from, high=x_to, size=dimensions)
-        self.fitness = 1 / self.func(self.position)
+        self.fitness = self.func(self.position)
+        self.a = a
+        self.c = c
         # c*I^a
-        self.fragrance = c * self.fitness ** a
+        self.fragrance = self.c * self.fitness ** self.a
 
     def step(self, best_solution, position_jth, position_kth, p):
-        x = np.random.uniform(0, 1)
-        if x > p:
-            z = np.random.uniform(0, 1)
-            self.position = self.position + np.subtract(
-                z ** 2 * best_solution, self.position) * self.fragrance
+        r = np.random.uniform(0, 1)
+        if r > p:
+            self.position = self.position + (r ** 2 * best_solution - self.position) * self.fragrance
         else:
-            z = np.random.uniform(0, 1)
-            self.position = self.position + np.subtract(
-                z ** 2 * position_jth.position, position_kth.position) * self.fragrance
+            self.position = self.position + (r ** 2 * position_jth.position - position_kth.position) * self.fragrance
 
-        self.fitness = 1 / self.func(self.position)
-        self.fragrance = c * self.fitness ** a
+        self.fitness = self.func(self.position)
+        self.fragrance = self.c * self.fitness ** self.a
         score = np.copy(self.fragrance)
-        return score
+        return score, self.position
 
 
 class Swarm:
-    def __init__(self, func, dimension, x_from: int, x_to: int, butterflies_number: int, p: float):
+    def __init__(self, func, dimension, x_from: int, x_to: int, butterflies_number: int, p: float, a: float, c: float):
         self.butterflies_number = butterflies_number
         self.p = p
-        self.butterflies = [Butterfly(func, dimension, x_from, x_to) for _ in range(butterflies_number)]
+        self.butterflies = [Butterfly(func, dimension, x_from, x_to, a, c) for _ in range(butterflies_number)]
         self.best_scores = np.copy(self.butterflies[0].fragrance)
+        self.best_position = np.copy(self.butterflies[0].position)
 
     def step(self, iteration):
-        edge = self.butterflies_number - 3
         for i in range(iteration):
-            x = 0
             for butterfly in self.butterflies:
-                score = butterfly.step(self.best_scores, self.butterflies[x + 1],
-                                       self.butterflies[x + 2], self.p)
+                j = random.randint(0, self.butterflies_number - 1)
+                k = random.randint(0, self.butterflies_number - 1)
+                score, position = butterfly.step(self.best_position, self.butterflies[j], self.butterflies[k], self.p)
 
-                x = x + 1
                 if score < self.best_scores:
                     self.best_scores = score
-                if x == edge:
-                    x = 0
+                    self.best_position = position
+
+            print(f'Iteration: {i}, Score: {self.best_scores}')
 
         return self.best_scores
 
 
 if __name__ == '__main__':
-    swarm = Swarm(sphere_func, 20, 10, 100, 100, 0.9)
+    c = 0.001
+    a = 0.1
+    swarm = Swarm(sphere_func, 20, -100, 100, 100, 0.5, a, c)
     x = swarm.step(10000)
     print(x)
